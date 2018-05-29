6cc2efea69d2894162cf7d5cbcbffa7d9f45c88c
DESKTOP-BELVMNP\yy <596257180@qq.com>
2018-05-29 23:23:36 +0800

    seventh commit

--- hstc_Lite/pages/publish/publish.js
+++ hstc_Lite/pages/publish/publish.js
@@ -1,18 +1,14 @@
 // pages/publish/publish.js
+var newsAll = require("../../data/homedata.js");
 Page({
 
-  /**
-   * 页面的初始数据
-   */
+
   data: {
   
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-  
+
   },
 
   /**
@@ -22,41 +18,45 @@ Page({
   
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+//选择本地图片并上传
+  uploadImg:function() {
+    var _this=this
+    
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths
+        // console.log(tempFilePaths[0])
+
+       _this.setData({
+         imgSrc:tempFilePaths,
+       })
+        
+        //上传图片
+       wx.uploadFile({
+         url: 'http://example.weixin.qq.com/upload', //地址
+         filePath: tempFilePaths[0],//图片临时目录
+         name: 'file',
+         formData: {
+           'user': 'test'
+         },
+         success: function (res) {
+           var data = res.data
+           //do something
+         }
+       })
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  formSubmit: function (e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
+ 
   /**
    * 用户点击右上角分享
    */
